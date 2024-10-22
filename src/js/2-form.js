e81cd2620ba84cac24ee483435a4b4dbca03e5d0
artem-ilchuk <pmumart@gmail.com>
2024-10-22 11:21:31 +0300

    hw-js_09

---
+++ src/js/2-form.js
@@ -0,0 +1,36 @@
+let formData = { email: '', message: '' };
+const LS_Key = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const formEmail = document.querySelector('.block text box');
+const formMessage = document.querySelector('.block text box textarea');
+
+const localData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (localStorage.getItem('feedback-form-state') !== null) {
+  formMessage.value = localData.message;
+  formEmail.value = localData.email;
+}
+form.addEventListener('input', event => {
+  if (event.target.nodeName === 'INPUT') {
+    formData.email = event.target.value;
+  }
+  if (event.target.nodeName === 'TEXTAREA') {
+    formData.message = event.target.value;
+  }
+
+  localStorage.setItem(LS_Key, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(LS_Key);
+    formData.email = '';
+    formData.message = '';
+    form.reset();
+  }
+});
